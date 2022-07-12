c85a90ecb4d9dcec08e386b7f9378229e9660d07
Rodrigo P. Capucho <xpiolx@hotmail.com>
2022-07-11 23:53:14 -0300

    primeiro commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Ola Mundo</h1>
+  return (
+    <>
+      <h1>Ola Mundo</h1>
+      <NotificationButton />
+    </>
   )
 }
 
